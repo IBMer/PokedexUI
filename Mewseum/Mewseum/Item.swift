e73c8d9658b6e10621ef39dda551fea429461cdc
Vincent WANG <vincent.x.h.wang@hotmail.com>
2025-11-18 16:59:45 +0800

    add iniital Mewseum Xcode project

---
+++ Mewseum/Mewseum/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Mewseum
+//
+//  Created by Vincent WANG on 2025/11/18.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Mewseum/Mewseum/MewseumApp.swift
@@ -0,0 +1,32 @@
+//
+//  MewseumApp.swift
+//  Mewseum
+//
+//  Created by Vincent WANG on 2025/11/18.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MewseumApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
